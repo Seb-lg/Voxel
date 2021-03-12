55f29a9401b7034b7e40d6e53a16548cb66690d2
Seub <seub@localhost.localdomain>
2021-03-12 16:49:49 +0100

    buble behavior and chunk fix ?

--- Components/DynamicPixels.cpp
+++ Components/DynamicPixels.cpp
@@ -17,21 +17,32 @@ Water::Water(sf::Vector2i globalIdx):
 {};
 
 void Water::update(Surrounding surround, sf::Vector2<int> pos, sf::Vector2<int> chunk_pos) {
-    if (processed)
+    bool forceUpdate = std::rand()%2;
+    if (processed && forceUpdate)
         return;
     auto density = (*surround.c)->density;
-    if (surround.d && density > (*surround.d)->density) {
+    if (surround.d && (!(*surround.d)->processed || forceUpdate) && (*surround.d)->type != type && density > (*surround.d)->density) {
+        (*surround.d)->processed = true;
+        processed = true;
         swapTiles(surround.c, surround.d, pos + sf::Vector2i(0, 1), chunk_pos);
-    } else if (surround.dl && density > (*surround.dl)->density) {
+    } else if (surround.dl && (!(*surround.dl)->processed || forceUpdate) && (*surround.dl)->type != type && density > (*surround.dl)->density) {
+        (*surround.dl)->processed = true;
+        processed = true;
         swapTiles(surround.c, surround.dl, pos + sf::Vector2i(-1, 1), chunk_pos);
-    } else if (surround.dr && density > (*surround.dr)->density) {
+    } else if (surround.dr && (!(*surround.dr)->processed || forceUpdate) && (*surround.dr)->type != type && density > (*surround.dr)->density) {
+        (*surround.dr)->processed = true;
+        processed = true;
         swapTiles(surround.c, surround.dr, pos + sf::Vector2i(1, 1), chunk_pos);
-    } else if (surround.r && density > (*surround.r)->density) {
+    } else if (surround.r && (!(*surround.r)->processed || forceUpdate) && (*surround.r)->type != type && density > (*surround.r)->density) {
+        (*surround.r)->processed = true;
+        processed = true;
         swapTiles(surround.c, surround.r, pos + sf::Vector2i(1, 0), chunk_pos);
-    } else if (surround.l && density > (*surround.l)->density) {
+    } else if (surround.l && (!(*surround.l)->processed || forceUpdate) && (*surround.l)->type != type && density > (*surround.l)->density) {
+        (*surround.l)->processed = true;
+        processed = true;
         swapTiles(surround.c, surround.l, pos + sf::Vector2i(-1, 0), chunk_pos);
     }
-    processed = true;
+//    processed = true;
 }
 
 Sand::Sand(sf::Vector2i globalIdx):
