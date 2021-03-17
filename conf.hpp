b13f5209d9f461469fc321d014953d0c9c83a656
zodiac <jean-baptiste.dupuy@epitech.eu>
2021-03-17 18:57:00 +0100

    base conf change

--- conf.hpp
+++ conf.hpp
@@ -8,14 +8,14 @@
 static auto rd = [](){std::srand(std::time(nullptr));return std::rand();};
 static auto getTime = [](){return std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::system_clock::now().time_since_epoch()).count();};
 
-#define PIXEL_SIZE 4
+#define PIXEL_SIZE 2
 #define CHUNK_SIZE 64
 #define WIDTH 1024
 #define HEIGHT 1024
 #define FPS 0
 #define USE_FRAGMENT_SHADERS false
-#define USE_VERTEX_SHADERS true
-#define DRAW_TILE_DEBUG true
+#define USE_VERTEX_SHADERS false
+#define DRAW_TILE_DEBUG false
 #define DEBUG
 
 #ifdef DEBUG
